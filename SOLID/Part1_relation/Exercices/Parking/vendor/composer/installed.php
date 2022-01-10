6f9f88344b8bf3c71a18b3c6c61c4778f7195227
Antoine07 <antoine.lucsko@gmail.com>
2022-01-10 14:22:11 +0100

    parking

---
+++ SOLID/Part1_relation/Exercices/Parking/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '28ad34e6d9528fe1a385f60139bd614e9ffac13b',
+        'name' => 'antoine/parking',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'antoine/parking' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '28ad34e6d9528fe1a385f60139bd614e9ffac13b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
